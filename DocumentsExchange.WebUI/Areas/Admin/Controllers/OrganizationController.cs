e8780bce7c123abcd8cf8f649e89dc9c324e264a
Ruslan Sochinsky <Intra123>
2017-01-17 16:34:34 +0200

    added logic to orgs in admin panel

--- DocumentsExchange.WebUI/Areas/Admin/Controllers/OrganizationController.cs
+++ DocumentsExchange.WebUI/Areas/Admin/Controllers/OrganizationController.cs
@@ -1,18 +1,86 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Web.Mvc;
+using DocumentsExchange.BusinessLayer.Services.Interfaces;
+using DocumentsExchange.DataLayer.Entity;
 
 namespace DocumentsExchange.WebUI.Areas.Admin.Controllers
 {
     public class OrganizationController : Controller
     {
-        // GET: Organization
+
+        private readonly IOrganizationProvider _organizationProvider;
+
+
+        public OrganizationController(IOrganizationProvider organizationProvider)
+        {
+            _organizationProvider = organizationProvider;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var orgs = _organizationProvider.GetAll().Result;
+            return View(orgs);
         }
 
         public PartialViewResult Create()
         {
+            var org = new Organization() {CreatedDateTime = DateTime.UtcNow};
+            return PartialView(org);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public PartialViewResult Create(Organization org)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = _organizationProvider.Add(org).Result;
+                }
+                else
+                {
+                    return PartialView();
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Невозможно сохранить изменения. Попробуйте позже.");
+            }
+
             return PartialView();
+        }
+
+
+        public PartialViewResult Edit(int orgId)
+        {
+            var org = _organizationProvider.Get(orgId).Result;
+            return PartialView(org);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Organization org)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = _organizationProvider.Update(org).Result;
+                }
+                else
+                {
+                    return PartialView();
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Невозможно сохранить изменения. Попробуйте позже.");
+            }
+
+            return RedirectToAction("Index","AdminPanel");
         }
     }
 }
