3ff9ffe27cee6b0cd80b5054b0f4e37297634227
Ruslan Sochinsky <Intra123>
2017-01-17 17:45:03 +0200

    added basic views for site control

---
+++ DocumentsExchange.WebUI/Areas/Admin/Controllers/ActivitySiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentsExchange.WebUI.Areas.Admin.Controllers
+{
+    public class ActivitySiteController : Controller
+    {
+        // GET: Admin/ActivitySite
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
